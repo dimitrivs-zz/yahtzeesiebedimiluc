2b5e0c8388204091efe5cf58384c642d94799a76
dimitrivansaelen <dimitrivansaelen@8342750e-2029-0410-9c91-534f36ee323b>
2007-02-26 09:44:52 +0000

    comments in yahtzee\mode\chat package

--- yahtzee/src/be/kdg/yahtzee/model/chat/Chat.java
+++ yahtzee/src/be/kdg/yahtzee/model/chat/Chat.java
@@ -10,20 +10,37 @@ package be.kdg.yahtzee.model.chat;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class provides chat functionality in the yahtzee application.
+ */
 public class Chat {
     private LinkedList<ChatMessage> chatMessages;
 
+    /**
+     * Constructor for chat application. Creates a linkedList in which the
+     * chat messages are stored.
+     */
     public Chat() {
         chatMessages = new LinkedList<ChatMessage>();
     }
 
+    /**
+     * Method for getting al the chat message stored in the linkedlist.
+     *
+     * @return List containing the chat messages.
+     */
+    public List getMessages() {
+        return chatMessages;
+    }
+
+    /**
+     * Method for adding a new chat message to the list.
+     *
+     * @param text String containing the message.
+     */
     public void addMessage(String text) {
         if (text != null) {
             chatMessages.addFirst(new ChatMessage(text));
         }
-    }
-
-    public List getMessages() {
-        return chatMessages;
     }
 }
