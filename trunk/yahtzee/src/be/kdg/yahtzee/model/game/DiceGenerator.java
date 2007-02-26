eb61d10c0d9c8a2dcecccd172235607a071ce721
dimitrivansaelen <dimitrivansaelen@8342750e-2029-0410-9c91-534f36ee323b>
2007-02-26 12:54:36 +0000

    comments in yahtzee\mode\game package

--- trunk/yahtzee/src/be/kdg/yahtzee/model/game/DiceGenerator.java
+++ trunk/yahtzee/src/be/kdg/yahtzee/model/game/DiceGenerator.java
@@ -10,21 +10,47 @@ package be.kdg.yahtzee.model.game;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * This class provides a generator for creating 5 dice for the yahtzee application.
+ */
 public class DiceGenerator {
     private List<Die> diceList;
 
+    /**
+     * Constructor for the dice generator.
+     * Creates a new List for storing the dice and fills the list with 5 new dice.
+     */
     public DiceGenerator() {
         diceList = new ArrayList<Die>();
 
         fillDiceList();
     }
 
+    /**
+     * Method for getting the list of dice.
+     *
+     * @return List containing the generated dice.
+     */
+    public List<Die> getDice() {
+        return diceList;
+    }
+
+    /**
+     * Method for rolling the dice.
+     */
     public void rollDice() {
         for (Die die : diceList) {
             die.roll();
         }
     }
 
+    /**
+     * Method for fixing a die, so it will not change when the dices are rolled.
+     *
+     * @param dieToFix Die object to fix
+     * @return boolean value
+     *         true if fixing has succeeded
+     */
     public boolean fixDie(Die dieToFix) {
         int dieIndex = diceList.indexOf(dieToFix);
         Die die = diceList.get(dieIndex);
@@ -33,6 +59,13 @@ public class DiceGenerator {
         return true;
     }
 
+    /**
+     * Method for unfixing a die, so it can change when the dices are rolled.
+     *
+     * @param dieToUnfix Die object to unfix
+     * @return boolean value
+     *         true if unfixing has succeeded
+     */
     public boolean unfixDie(Die dieToUnfix) {
         int dieIndex = diceList.indexOf(dieToUnfix);
         Die die = diceList.get(dieIndex);
@@ -41,16 +74,22 @@ public class DiceGenerator {
         return true;
     }
 
-    public List<Die> getDice() {
-        return diceList;
-    }
-
+    /**
+     * Method for resetting the dicegenerator.
+     * The list is cleared and filled with new dices.
+     *
+     * @return boolean value
+     *         true if reset has succeeded
+     */
     public boolean resetDiceGenerator() {
         diceList.clear();
         fillDiceList();
         return true;
     }
 
+    /**
+     * Method for filling the dicelist with 5 new dices.
+     */
     private void fillDiceList() {
         for (int i = 0; i < 5; i++) {
             diceList.add(new Die(i));
