632b6310ddff3fe19899502a71daadb716c081d0
dimitrivansaelen <dimitrivansaelen@8342750e-2029-0410-9c91-534f36ee323b>
2007-02-26 13:01:15 +0000

    comments in highscore

--- yahtzee/src/be/kdg/yahtzee/model/game/Highscore.java
+++ yahtzee/src/be/kdg/yahtzee/model/game/Highscore.java
@@ -9,6 +9,9 @@ package be.kdg.yahtzee.model.game;
 
 import be.kdg.yahtzee.model.users.User;
 
+/**
+ * Class representing a Highscore object in the yahtzee game
+ */
 public class Highscore {
     private int highscoreId;
     private int score;
@@ -17,32 +20,69 @@ public class Highscore {
     public Highscore() {
     }
 
+    /**
+     * Constructor for a hoghscore object
+     *
+     * @param highscoreId Integer id of the highscore.
+     * @param user        User object owning the highscore.
+     * @param score       Score object representing the user's score.
+     */
     public Highscore(int highscoreId, User user, int score) {
         this.highscoreId = highscoreId;
         this.user = user;
         this.score = score;
     }
 
+    /**
+     * Method for getting the id of a highscore.
+     *
+     * @return Integer value representing highscore id.
+     */
     public int getHighscoreId() {
         return highscoreId;
     }
 
+    /**
+     * Method for setting the id of a highscore.
+     *
+     * @param highscoreId Integer value representing highscore id.
+     */
     public void setHighscoreId(int highscoreId) {
         this.highscoreId = highscoreId;
     }
 
+    /**
+     * Method for getting the User of a highscore.
+     *
+     * @return User object representing owner of highscore.
+     */
     public User getUser() {
         return user;
     }
 
+    /**
+     * Method for setting the User of a highscore.
+     *
+     * @param user User object representing owner of highscore.
+     */
     public void setUser(User user) {
         this.user = user;
     }
 
+    /**
+     * Method for getting the Score value of a highscore.
+     *
+     * @return Integer value representing highscore.
+     */
     public int getScore() {
         return score;
     }
 
+    /**
+     * Method for setting the Score value of a highscore.
+     *
+     * @param score Integer value representing highscore.
+     */
     public void setScore(int score) {
         this.score = score;
     }
