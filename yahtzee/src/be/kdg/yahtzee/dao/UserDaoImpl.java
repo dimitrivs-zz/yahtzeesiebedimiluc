e353c6156fbe6510c7218969ec44fdee56309b35
dimitrivansaelen <dimitrivansaelen@8342750e-2029-0410-9c91-534f36ee323b>
2007-02-26 09:23:47 +0000

    comments in yahtzee\dao package

--- yahtzee/src/be/kdg/yahtzee/dao/UserDaoImpl.java
+++ yahtzee/src/be/kdg/yahtzee/dao/UserDaoImpl.java
@@ -14,33 +14,62 @@ import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
 
 import java.util.List;
 
+/**
+ * Implementation of the UserDao interface.
+ * This class provides database connectivity for accessing user data.
+ */
 public class UserDaoImpl extends HibernateDaoSupport implements UserDao {
-    //private HibernateTemplate hibernateTemplate;
 
     public UserDaoImpl() {
-        //this.hibernateTemplate = getHibernateTemplate();
     }
 
+    /**
+     * Method for getting a user by an id.
+     *
+     * @param userId Id of the wanted user.
+     * @return User object
+     */
     public User getUser(int userId) {
         HibernateTemplate hibernateTemplate = getHibernateTemplate();
         return (User) hibernateTemplate.get(User.class, userId);
     }
 
-    public void saveUser(User user) {
-        HibernateTemplate hibernateTemplate = getHibernateTemplate();
-        hibernateTemplate.saveOrUpdate(user);
-    }
-
-    public void deleteUser(User user) {
-        HibernateTemplate hibernateTemplate = getHibernateTemplate();
-        hibernateTemplate.delete(user);
-    }
-
+    /**
+     * Method for getting a List of all the users in the database.
+     *
+     * @return List containing all the users in the database.
+     */
     public List<User> getUsers() {
         HibernateTemplate hibernateTemplate = getHibernateTemplate();
         return hibernateTemplate.find("from User");
     }
 
+    /**
+     * Method for saving a user to the database.
+     *
+     * @param user User object to be saved in the database.
+     */
+    public void saveUser(User user) {
+        HibernateTemplate hibernateTemplate = getHibernateTemplate();
+        hibernateTemplate.saveOrUpdate(user);
+    }
+
+    /**
+     * Method or deleting a user from the database.
+     *
+     * @param user User object to be deleted from the database.
+     */
+    public void deleteUser(User user) {
+        HibernateTemplate hibernateTemplate = getHibernateTemplate();
+        hibernateTemplate.delete(user);
+    }
+
+    /**
+     * Method for creating a HibernateTemplate for accessing a database in a session.
+     *
+     * @param sessionFactory SessionFactory object containing database session.
+     * @return HibernateTemplate object
+     */
     public HibernateTemplate createHibernateTemplate(SessionFactory sessionFactory) {
         return super.createHibernateTemplate(sessionFactory);    //To change body of overridden methods use File | Settings | File Templates.
     }
