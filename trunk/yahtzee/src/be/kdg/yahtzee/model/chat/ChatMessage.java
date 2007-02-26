63d9518020d02d0698dbfdc0ccdf955f0bc520d5
dimitrivansaelen <dimitrivansaelen@8342750e-2029-0410-9c91-534f36ee323b>
2007-02-26 09:44:52 +0000

    comments in yahtzee\mode\chat package

--- trunk/yahtzee/src/be/kdg/yahtzee/model/chat/ChatMessage.java
+++ trunk/yahtzee/src/be/kdg/yahtzee/model/chat/ChatMessage.java
@@ -7,9 +7,18 @@
 
 package be.kdg.yahtzee.model.chat;
 
+/**
+ * This class represents a chat message in the chat application.
+ */
 public class ChatMessage {
     private String text;
 
+    /**
+     * Constructor for a new chat message.
+     * Only the 256 first characters are saved.
+     *
+     * @param newtext String containing the text of the message.
+     */
     public ChatMessage(String newtext) {
         text = newtext;
         if (text.length() > 256) {
@@ -17,6 +26,11 @@ public class ChatMessage {
         }
     }
 
+    /**
+     * Method for getting the text of the message.
+     *
+     * @return String containing the text of the message.
+     */
     public String getText() {
         return text;
     }
