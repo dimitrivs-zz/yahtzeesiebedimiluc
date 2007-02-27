60817acff19d631d90deaf482cb02c15b83caf0c
dimitrivansaelen <dimitrivansaelen@8342750e-2029-0410-9c91-534f36ee323b>
2007-02-27 11:59:35 +0000

    gameGameServlet aangemaakt

---
+++ yahtzee/src/be/kdg/yahtzee/servlets/game/FinishGameServlet.java
@@ -0,0 +1,28 @@
+package be.kdg.yahtzee.servlets.game;
+
+import be.kdg.yahtzee.beans.UserBean;
+import be.kdg.yahtzee.model.YahtzeeController;
+import be.kdg.yahtzee.model.users.User;
+import be.kdg.yahtzee.servlets.YahtzeeServlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class FinishGameServlet extends YahtzeeServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String gameName = request.getParameter("game");
+
+        YahtzeeController yahtzeeController = findYahtzeeController();
+
+        HttpSession session = request.getSession();
+        UserBean userBean = (UserBean) session.getAttribute("userBean");
+        User user = yahtzeeController.findUser(userBean.getUsername());
+
+        yahtzeeController.leaveGame(gameName, user);
+
+        response.sendRedirect("/faces/player/gameRoom.jsp");
+    }
+}
