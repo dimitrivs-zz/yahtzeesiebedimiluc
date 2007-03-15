5f27656d474b6dce3184659c41b2dd75ef383379
schillemansluc <schillemansluc@8342750e-2029-0410-9c91-534f36ee323b>
2007-03-15 13:22:18 +0000

    Godklasse Game opgesplitst

--- yahtzee/src/be/kdg/yahtzee/model/game/Game.java
+++ yahtzee/src/be/kdg/yahtzee/model/game/Game.java
@@ -7,7 +7,6 @@
 
 package be.kdg.yahtzee.model.game;
 
-import static be.kdg.util.ScorePoints.*;
 import be.kdg.yahtzee.model.chat.Chat;
 import be.kdg.yahtzee.model.users.User;
 
@@ -24,15 +23,14 @@ public class Game {
     private EnumState state;
     private Chat chat;
     private List<Die> diceList = new ArrayList<Die>();
-    private List<Integer> diceValueList = new ArrayList<Integer>();
-    private List<Integer> diceValueListWithoutDoubles;
     private DiceGenerator diceGen;
-    private int points;
     private User activePlayer;
     private Map<String, Score> scores;
     private List<User> userList;
     private Map<String, Integer> possibleScores;
     private int numberOfRolls;
+
+    private ScoreCalculator scoreCalculator;
 
     //Nodig voor testGame - tijdelijk
 
@@ -64,6 +62,7 @@ public class Game {
         diceGen = new DiceGenerator();
         possibleScores = new HashMap<String, Integer>();
         numberOfRolls = 0;
+        scoreCalculator = new ScoreCalculator();
     }
 
     /**
@@ -219,7 +218,7 @@ public class Game {
     private void resetRound() {
         numberOfRolls = 0;
         diceList.clear();
-        diceValueList.clear();
+        scoreCalculator.clearDiceValueList();
         possibleScores.clear();
         diceGen.resetDiceGenerator();
     }
@@ -249,17 +248,6 @@ public class Game {
             calculateScores();
         }
         return diceList;
-    }
-
-    /**
-     * Method for filling a List with dice values.
-     */
-    private void fillDiceValues() {
-        //diceValueList = new ArrayList<Integer>();
-        diceValueList.clear();
-        for (Die die : diceList) {
-            diceValueList.add(die.getValue());
-        }
     }
 
     /**
@@ -343,302 +331,7 @@ public class Game {
      * Calculate the yahtzee scores.
      */
     public void calculateScores() {
-        fillDiceValues();
-        Collections.sort(diceValueList);
-
-        System.out.println(diceValueList);
-
-        calculateOnes();
-        calculateTwos();
-        calculateThrees();
-        calculateFours();
-        calculateFives();
-        calculateSixes();
-
-        calculateThreeOfAKind();
-        calculateCarre();
-        calculateFullHouse();
-        calculateSmallStreet();
-        calculateLargeStreet();
-        calculateYahtzee();
-        calculateYahtzeeBonus();
-        calculateChance();
-    }
-
-    /**
-     * Calculate Ones score.
-     */
-    private void calculateOnes() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isOnesFixed()) {
-            for (Die die : diceList) {
-                if (die.getValue() == 1) {
-                    points += 1;
-                }
-            }
-            possibleScores.put("ones", points);
-        }
-    }
-
-    /**
-     * Calculate Twos score.
-     */
-    private void calculateTwos() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isTwosFixed()) {
-            for (Die die : diceList) {
-                if (die.getValue() == 2) {
-                    points += 2;
-                }
-            }
-            possibleScores.put("twos", points);
-        }
-    }
-
-    /**
-     * Calculate Threes score.
-     */
-    private void calculateThrees() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isThreesFixed()) {
-            for (Die die : diceList) {
-                if (die.getValue() == 3) {
-                    points += 3;
-                }
-            }
-            possibleScores.put("threes", points);
-        }
-    }
-
-    /**
-     * Calculate Fours score.
-     */
-    private void calculateFours() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isFoursFixed()) {
-            for (Die die : diceList) {
-                if (die.getValue() == 4) {
-                    points += 4;
-                }
-            }
-            possibleScores.put("fours", points);
-        }
-    }
-
-    /**
-     * Calculate Fives score.
-     */
-    private void calculateFives() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isFivesFixed()) {
-            for (Die die : diceList) {
-                if (die.getValue() == 5) {
-                    points += 5;
-                }
-            }
-            possibleScores.put("fives", points);
-        }
-    }
-
-    /**
-     * Calculate Sixes score.
-     */
-    private void calculateSixes() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isSixesFixed()) {
-            for (Die die : diceList) {
-                if (die.getValue() == 6) {
-                    points += 6;
-                }
-            }
-            possibleScores.put("sixes", points);
-        }
-    }
-
-
-    /**
-     * Calculate ThreeOfAKind score.
-     */
-    private void calculateThreeOfAKind() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isThreeOfAKindFixed()) {
-            if (diceValueList.get(0) == diceValueList.get(1) &&
-                    diceValueList.get(1) == diceValueList.get(2)) {
-                points = countAll();
-            } else {
-                if (diceValueList.get(1) == diceValueList.get(2) &&
-                        diceValueList.get(2) == diceValueList.get(3)) {
-                    points = countAll();
-                } else {
-                    if (diceValueList.get(2) == diceValueList.get(3) &&
-                            diceValueList.get(3) == diceValueList.get(4)) {
-                        points = countAll();
-                    }
-                }
-            }
-            possibleScores.put("threeOfAKind", points);
-        }
-    }
-
-    /**
-     * Calculate Carre score.
-     */
-    private void calculateCarre() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isCarreFixed()) {
-            if (diceValueList.get(0) == diceValueList.get(1) &&
-                    diceValueList.get(1) == diceValueList.get(2) &&
-                    diceValueList.get(2) == diceValueList.get(3)) {
-                points = countAll();
-            } else {
-                if (diceValueList.get(1) == diceValueList.get(2) &&
-                        diceValueList.get(2) == diceValueList.get(3) &&
-                        diceValueList.get(3) == diceValueList.get(4)) {
-                    points = countAll();
-                }
-            }
-            possibleScores.put("carre", points);
-        }
-    }
-
-    /**
-     * Calculate FullHouse score.
-     */
-    private void calculateFullHouse() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isFullHouseFixed()) {
-            if (diceValueList.get(0) == diceValueList.get(1) &&
-                    diceValueList.get(1) == diceValueList.get(2) &&
-                    diceValueList.get(3) == diceValueList.get(4)) {
-                points = FULLHOUSEPOINTS;
-            }
-            if (diceValueList.get(0) == diceValueList.get(1) &&
-                    diceValueList.get(2) == diceValueList.get(3) &&
-                    diceValueList.get(3) == diceValueList.get(4)) {
-                points = FULLHOUSEPOINTS;
-            }
-            possibleScores.put("fullHouse", points);
-        }
-    }
-
-    /**
-     * Calculate SmallStreet score.
-     */
-    private void calculateSmallStreet() {
-        points = 0;
-        removeDoubles();
-        if (!scores.get(activePlayer.getUsername()).isSmallStreetFixed()) {
-            if (diceValueListWithoutDoubles.size() >= 4) {
-                if (diceValueListWithoutDoubles.get(0) + 1 == diceValueListWithoutDoubles.get(1) &&
-                        diceValueListWithoutDoubles.get(1) + 1 == diceValueListWithoutDoubles.get(2) &&
-                        diceValueListWithoutDoubles.get(2) + 1 == diceValueListWithoutDoubles.get(3)) {
-                    points = SMALLSTREETPOINTS;
-                }
-            }
-            if (diceValueListWithoutDoubles.size() == 5) {
-                if (diceValueListWithoutDoubles.get(1) + 1 == diceValueListWithoutDoubles.get(2) &&
-                        diceValueListWithoutDoubles.get(2) + 1 == diceValueListWithoutDoubles.get(3) &&
-                        diceValueListWithoutDoubles.get(3) + 1 == diceValueListWithoutDoubles.get(4)) {
-                    points = SMALLSTREETPOINTS;
-                }
-            }
-            possibleScores.put("smallStreet", points);
-        }
-    }
-
-    /**
-     * Calculate LargeStreet score.
-     */
-    private void calculateLargeStreet() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isLargeStreetFixed()) {
-            if (diceValueList.get(0) + 1 == diceValueList.get(1) &&
-                    diceValueList.get(1) + 1 == diceValueList.get(2) &&
-                    diceValueList.get(2) + 1 == diceValueList.get(3) &&
-                    diceValueList.get(3) + 1 == diceValueList.get(4)) {
-                points = LARGESTREETPOINTS;
-            }
-
-            possibleScores.put("largeStreet", points);
-        }
-    }
-
-    /**
-     * Calculate Yahtzee score.
-     */
-    private void calculateYahtzee() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isYahtzeeFixed()) {
-            if (matchDice(0, 1) && matchDice(1, 2) && matchDice(2, 3) && matchDice(3, 4)) {
-                points = YAHTZEEPOINTS;
-            }
-            possibleScores.put("yahtzee", points);
-        }
-    }
-
-    /**
-     * Calculate YahtzeeBonus score.
-     */
-    private void calculateYahtzeeBonus() {
-        points = 0;
-        if (scores.get(activePlayer.getUsername()).isYahtzeeFixed() && scores.get(activePlayer.getUsername()).getYahtzee() != 0) {
-            if (matchDice(0, 1) && matchDice(1, 2) && matchDice(2, 3) && matchDice(3, 4)) {
-                Score score = scores.get(activePlayer.getUsername());
-                score.addyahtzee();
-            }
-        }
-    }
-
-    /**
-     * Calculate Chance score.
-     */
-    private void calculateChance() {
-        points = 0;
-        if (!scores.get(activePlayer.getUsername()).isChanceFixed()) {
-            points = countAll();
-            possibleScores.put("chance", points);
-        }
-    }
-
-    /**
-     * Method for counting all dice values.
-     *
-     * @return Integer value with dice values.
-     */
-    private int countAll() {
-        for (Die die : diceList) {
-            points += die.getValue();
-        }
-        return points;
-    }
-
-    /**
-     * Method for matching two dice.
-     *
-     * @param aDieId Die 1
-     * @param bDieId Die 2
-     * @return boolean value if equal.
-     */
-    private boolean matchDice(int aDieId, int bDieId) {
-        Die aDie = getDie(aDieId);
-        Die bDie = getDie(bDieId);
-
-        return aDie.getValue() == bDie.getValue();
-    }
-
-    /**
-     * Method for removing doubles in a diceValueList.
-     */
-    private void removeDoubles() {
-        diceValueListWithoutDoubles = new ArrayList<Integer>();
-
-        for (int i = 0; i < 4; i++) {
-            if (diceValueList.get(i) != diceValueList.get(i + 1)) {
-                diceValueListWithoutDoubles.add(diceValueList.get(i));
-            }
-        }
-
-        diceValueListWithoutDoubles.add(diceValueList.get(4));
+        possibleScores = scoreCalculator.calculateScores(diceList, scores, activePlayer);
     }
 
     /**
